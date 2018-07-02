040f38305806f61b9e5b2c9550cf89e70da26d18
machenfeng <machenfeng@msn.cn>
2018-07-02 11:27:23 +0800

    Update SWEM_C.py

--- python/SWEM_C.py
+++ python/SWEM_C.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class Model(nn.Module):
@@ -27,30 +26,29 @@ class Model(nn.Module):
     def avgpool(self, x, mask4div):
         return torch.div(x.sum(1), mask4div)
 
-
     def forward(self, x, mask):
 
         bs = x.size(0)
-        #get mask
+        # get mask
         mask4div = mask.sum(1).unsqueeze(1).expand([bs, self.ed])
 
         mask4fill = abs(mask - 1).byte()
         mask4fill = mask4fill.unsqueeze(2).expand([bs, self.sl, self.ed])
 
-        #（bs, sl） -> (bs, sl, ed)
+        # (bs, sl) -> (bs, sl, ed)
         out = self.embed(x)
         out = out.masked_fill_(mask4fill, 0)
 
-        #(bs, sl, ed) -> (bs, 1, ed)
+        # (bs, sl, ed) -> (bs, 1, ed)
         max = self.maxpool(out)
         avg = self.avgpool(out, mask4div)
 
-        #(bs, 1, ed) -> (bs, ed * 2)
+        # (bs, 1, ed) -> (bs, ed * 2)
         max = max.squeeze(1)
         avg = avg.squeeze(1)
         out = torch.cat([max, avg], 1)
 
-        #(bs, ed * 2) -> (bs, nc)
+        # (bs, ed * 2) -> (bs, nc)
         logits = self.fc(out)
 
         return logits
