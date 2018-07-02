77f0d1354393db02eb7acb5d4ef68e9439e95050
machenfeng <machenfeng@msn.cn>
2018-07-02 11:27:03 +0800

    Update Inception.py

--- python/Inception.py
+++ python/Inception.py
@@ -1,32 +1,32 @@
 import torch
 import torch.nn as nn
 
-class Inception_block(nn.Module):
+
+class InceptionBlock(nn.Module):
 
     def __init__(self, args):
-        super(Inception_block, self).__init__()
+        super(InceptionBlock, self).__init__()
 
         oc = args.out_channels
         ed = args.embed_dim
 
-        #(bs, 1, sl, ed) -> (bs, oc, sl, 1)
+        # (bs, 1, sl, ed) -> (bs, oc, sl, 1)
         self.branch1 = nn.Conv2d(1, oc, (1, ed))
 
-        #(bs, 1, sl, ed) -> (bs, oc, sl-3+1, 1)
+        # (bs, 1, sl, ed) -> (bs, oc, sl-3+1, 1)
         self.branch2 = nn.Sequential(nn.Conv2d(1, oc, (1, ed)),
                                      nn.ReLU(),
                                      nn.Conv2d(oc, oc, (3, 1)))
 
-        #(bs, 1, sl, ed) -> (bs, oc, sl-3+1-5+1, 1)
+        # (bs, 1, sl, ed) -> (bs, oc, sl-3+1-5+1, 1)
         self.branch3 = nn.Sequential(nn.Conv2d(1, oc, (3, ed)),
                                      nn.ReLU(),
                                      nn.Conv2d(oc, oc, (5, 1)))
-        #(bs, 1, sl, ed) -> (bs, oc, sl-2, 1)
+        # (bs, 1, sl, ed) -> (bs, oc, sl-2, 1)
         self.branch4 = nn.Conv2d(1, oc, (3, ed))
 
         self.relu = nn.ReLU()
         self.bn = nn.BatchNorm1d(oc)
-
 
     def forward(self, x):
 
@@ -55,23 +55,22 @@ class Model(nn.Module):
 
         self.embed = nn.Embedding(vs, ed)
 
-        self.inception_block = Inception_block(args)
+        self.inception_block = InceptionBlock(args)
 
         self.fc = nn.Sequential(nn.Linear(oc * (sl * 4 - 10), hs),
                                 nn.BatchNorm1d(hs),
                                 nn.ReLU(),
                                 nn.Linear(hs, nc))
 
-
     def forward(self, x, _):
 
-        #(bs, sl) -> (bs, 1， sl， ed)
+        # (bs, sl) -> (bs, 1， sl， ed)
         out = self.embed(x).unsqueeze(1)
 
-        #(bs, 1， sl， ed) -> (bs, oc, (sl * 4 - 10))
+        # (bs, 1， sl， ed) -> (bs, oc, (sl * 4 - 10))
         out = self.inception_block(out)
 
-        #(bs, oc, (sl * 4 - 10)) -> (bs, nc)
+        # (bs, oc, (sl * 4 - 10)) -> (bs, nc)
         out = out.view(out.size(0), -1)
         logits = self.fc(out)
 
